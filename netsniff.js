494525b183388b48a93c86e44fe32e199617fbca
Petter Reinholdtsen <pere@hungry.com>
2015-02-12 23:12:08 +0100

    Avoid hanging forever.

--- netsniff.js
+++ netsniff.js
@@ -1,3 +1,7 @@
+// from http://stackoverflow.com/questions/16846506/phantomjs-timeout
+// stop after 20 sec ( add this before you request your webpage )
+setTimeout(function() {phantom.exit();},20000);
+
 if (!Date.prototype.toISOString) {
     Date.prototype.toISOString = function () {
         function pad(n) { return n < 10 ? '0' + n : n; }
